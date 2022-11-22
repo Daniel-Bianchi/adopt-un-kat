f1e1a33d7ee355facbd9138fad9a205f0bf9e4c8
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-11-22 12:17:48 +0100

    add delete method

--- backend-kitten/src/main/java/com/wsc/backendkitten/controllers/KittenController.java
+++ backend-kitten/src/main/java/com/wsc/backendkitten/controllers/KittenController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,8 +63,12 @@ public class KittenController {
         return kittenRepository.save(kittenToUpdate);
     }
 
-
     //Supprimer un chat
+    @DeleteMapping
+    public boolean delete(@PathVariable int id) {
+        kittenRepository.deleteById(id);
+        return true;
+    }
 
     
 }
