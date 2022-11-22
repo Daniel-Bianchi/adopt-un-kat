0351ac4d52da59393e0c1e71eaca3d6395fb278c
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-11-22 11:57:54 +0100

    add post method

--- backend-kitten/src/main/java/com/wsc/backendkitten/controllers/KittenController.java
+++ backend-kitten/src/main/java/com/wsc/backendkitten/controllers/KittenController.java
@@ -1,10 +1,14 @@
 package com.wsc.backendkitten.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +23,22 @@ public class KittenController {
     @Autowired
     KittenRepository kittenRepository;
 
+    //Liste des chat en fonction de leur statut d'adoption
     @GetMapping
     public List<Kitten> findAllKittenByIsAdopted(Boolean isAdopted) {
         return this.kittenRepository.findAllKittenByIsAdopted(isAdopted); 
+    }
+
+    //Un chat par son id
+    @GetMapping("/{id}")
+    public Optional<Kitten> show(@PathVariable int id) {
+        return kittenRepository.findById(id);
+    }
+
+    //Ajouter un chat
+    @PostMapping
+    public Kitten create(@RequestBody Kitten newKitten) {
+        return kittenRepository.save(newKitten);
     }
     
 }
